500ed310d52937d24032d4a16efa6461ed3e2a2b
sunmc666 <2746640301@qq.com>
2022-09-21 14:44:06 +0800

    add dist document

--- cherry-richtext/cherry/cherryRichtextFlag.js
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (c) Tiny Technologies, Inc. All rights reserved.
- * Licensed under the LGPL or a commercial license.
- * For LGPL see License.txt in the project root for license information.
- */
-
-window.hasCherryRichtextResource = true;

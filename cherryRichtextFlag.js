c366929025e65f74c2267efdddd674107907678b
sunmc666 <2746640301@qq.com>
2024-05-29 10:21:15 +0800

    update dist.zip

--- cherryRichtextFlag.js
+++ cherryRichtextFlag.js
@@ -3,5 +3,4 @@
  * Licensed under the LGPL or a commercial license.
  * For LGPL see License.txt in the project root for license information.
  */
-const hasCherryRichtextResource = true;
-window.hasCherryRichtextResource = hasCherryRichtextResource;
+window.hasCherryRichtextResource = true;
